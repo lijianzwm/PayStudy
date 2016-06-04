02b0d4a2e20cfd9486ca5fb65e10efcb940e4d2f
lijianzwm <lijianzhanwuming@163.com>
2016-06-04 11:49:19 +0800

    Init Commit

---
+++ ThinkPHP/Library/Vendor/WeChatPay/WxPayService.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 16/6/4
+ * Time: 上午11:28
+ */
+
+require_once "WxPay.Api.php";
+require_once "WxPay.NativePay.php";
+
+class WeChatPayService{
+
+    public static function QRCode( $product ){
+        $notify = new NativePay();
+        $input = new WxPayUnifiedOrder();
+        $input->SetProduct_id($product['product_id']);
+        $input->SetGoods_tag($product['goods_tag']);
+        $input->SetBody($product['body']);
+        $input->SetAttach($product['attach']);
+        $input->SetTotal_fee($product['fee']);
+        $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+        $input->SetTime_start(date("YmdHis"));
+        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+        $input->SetTrade_type("NATIVE");
+        dump($input);
+        $result = $notify->GetPayUrl($input);
+        $url2 = $result["code_url"];
+        dump($result);
+        return "http://paysdk.weixin.qq.com/example/qrcode.php?data=".urlencode($url2);
+    }
+
+
+}
+
+
